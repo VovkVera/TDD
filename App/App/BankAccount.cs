1491124dec9ca5c4c4258556949972d8d9cf05f7
Uraganchik1983 <capability@capability.org.ua>
2019-02-23 08:44:26 +0200

    @vovkvera  added example UnitTest
    @vovkvera  added example UnitTest for Bank Account

---
+++ App/App/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class BankAccount
+    {
+        string _customerName;
+        double _balace;
+
+        public BankAccount(string customerName, double balance)
+        {
+            _customerName = customerName;
+            _balace = balance;
+        }
+        
+        public double Balance { get { return _balace; } }
+
+        public void Debit (double amount)
+        {
+            if (_balace == 0)
+                throw new Exception("balance = 0");
+            if (amount <= 0 || amount > _balace)
+                throw new ArgumentOutOfRangeException("amount <=0 or amount > balance");
+            _balace -= amount;
+
+        }
+            public void  Credit (double amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException("amount <=0");
+
+        }
+    }
+}
---
+++ App/AppTests/BankAccountTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        public void TestExceptionCase()
+        {
+            BankAccount bank = new BankAccount("Vera", 0);
+            try
+            {
+                bank.Debit(2);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, "amount <=0 or amount > balance");
+                return;
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "balane =0");
+                return;
+            }
+            Assert.Fail("No exception was throw");
+        }
+
+        [TestMethod()]
+
+        public void TestCreditCase()
+        {
+            BankAccount bank = new BankAccount("Vera", 2);
+            bank.Credit(2);
+            Assert.AreEqual(0, bank.Balance);
+        }
+
+        [TestMethod()]
+        public void TestDebitCase()
+        {
+            BankAccount bank = new BankAccount("Vera", 2);
+            bank.Debit(2);
+            Assert.AreEqual(0, bank.Balance);
+        }
+
+        [TestMethod()]
+        public void TestCreditDebitCase()
+        {
+            BankAccount bank = new BankAccount("Vera", 2);
+            bank.Debit(2);
+            bank.Credit(2);
+            Assert.AreEqual(2, bank.Balance);
+        }
+    }
+}
