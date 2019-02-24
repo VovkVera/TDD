39366a2468a0a1939eec021b10542ac62b88bd2a
Uraganchik1983 <capability@capability.org.ua>
2019-02-24 05:00:13 +0200

    GoogleTestDemo
    @vovkvera fix C4996

---
+++ GoogleTestDemo/GoogleTestDemo/GoogleTestDemo.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include <iostream>
+// simple test example for students
+
+#include <gtest/gtest.h>
+struct  BankAccount
+{
+	int balance;
+	BankAccount()
+	{
+	}
+
+	explicit BankAccount(const int balance)
+		:balance{ balance }
+	{
+
+	}
+};
+
+TEST(AccountTest, BankAccountStartsEmpty)
+{
+	BankAccount account;
+	EXPECT_EQ(0, account.balance);
+
+}
+int main(int argc, char* argv[])
+{
+
+	std::cout << "Hello World!\n";
+	testing::InitGoogleTest(&argc, argv);
+}
