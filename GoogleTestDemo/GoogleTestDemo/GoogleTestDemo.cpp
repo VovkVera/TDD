6f76e910d1b2a167cb7eb86dba5c2dcc59f92ec4
Vera Vovk <vovkvera@gmail.com>
2019-03-04 09:15:22 +0200

    Update GoogleTestDemo.cpp

--- GoogleTestDemo/GoogleTestDemo/GoogleTestDemo.cpp
+++ GoogleTestDemo/GoogleTestDemo/GoogleTestDemo.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include <iostream>
 // simple test example
 
@@ -7,9 +7,7 @@ struct  BankAccount
 {
 <<<<<<< HEAD
 	int balance=0;
-=======
-	int balance = 0;
->>>>>>> 1afa2df0e16be6e2d8a56be3b4edb82817edf87c
+	
 	BankAccount()
 	{
 	}
